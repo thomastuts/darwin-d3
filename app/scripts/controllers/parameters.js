35a3c4dba610d964a945e6b60788fde8b765de85
Thomas Tuts <thomas.tuts@gmail.com>
2013-10-17 22:19:17 +0200

    - Watch parameter service for changes and update graph accordingly - Implement source parameter selection

---
+++ app/scripts/controllers/parameters.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('darwinD3App')
+  .controller('ParametersCtrl', function ($scope, Data, Parameters) {
+    $scope.params = Parameters.params;
+
+    Data.getData().then(function (result) {
+      $scope.availableDates = _.uniq(_.pluck(result.data, 'period')).reverse();
+    });
+
+    $scope.toggleNetwork = function (network) {
+      if (_.contains($scope.params.selectedNetworks, network)) {
+        $scope.params.selectedNetworks = _.without($scope.params.selectedNetworks, network);
+      }
+      else {
+        $scope.params.selectedNetworks.push(network);
+      }
+    };
+
+    $scope.isSelectedNetwork = function (network) {
+      return _.contains($scope.params.selectedNetworks, network);
+    };
+
+  });
--- app/scripts/services/parameters.js
+++ app/scripts/services/parameters.js
@@ -4,6 +4,16 @@ angular.module('darwinD3App')
   .service('Parameters', function Parameters() {
     var params = {};
 
+    // set default parameters
+    params.startDate = '2013-09-08';
+    params.endDate = '2013-09-30';
+
+    params.availableNetworks = ['facebook', 'twitter', 'website'];
+    params.selectedNetworks = ['facebook', 'twitter'];
+
+    params.availableMetrics = ['action', 'advocacy', 'awareness', 'appreciation'];
+    params.selectedMetric = 'advocacy';
+
     return {
       params: params
     };
