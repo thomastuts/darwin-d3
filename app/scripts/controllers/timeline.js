6d59fff92d05e1dbba8d5ac1aa0efe7f9551f394
Thomas Tuts <thomas.tuts@gmail.com>
2013-10-17 13:04:42 +0200

    Render timeline based on start/end date and update accordingly

--- app/scripts/controllers/timeline.js
+++ app/scripts/controllers/timeline.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('darwinD3App')
-  .controller('TimelineCtrl', function ($scope, Fixtures, Layout, Data) {
+  .controller('TimelineCtrl', function ($scope, Fixtures, Layout, Data, Parameters) {
     Data.getData().then(function (result) {
       $scope.uniqueDates = _.uniq(_.pluck(result.data, 'period')).reverse();
+      $scope.params = Parameters.params;
 
-      $scope.startDate = '2013-09-23';
+      $scope.startDate = '2013-09-08';
       $scope.endDate = '2013-09-30';
 
       $scope.visibleNetworks = ['facebook', 'twitter'];
@@ -42,7 +43,7 @@ angular.module('darwinD3App')
           return y(d.amount);
         });
 
-      var svg = d3.select('body').append('svg')
+      var svg = d3.select('#graph-timeline').append('svg')
         .attr({
           width: width + margin.left + margin.right,
           height: height + margin.top + margin.right
@@ -52,80 +53,164 @@ angular.module('darwinD3App')
           transform: 'translate(' + margin.left + ',' + margin.top + ')'
         });
 
-      var seriesNames = d3.keys($scope.dataset[0])
-        .filter(function (d) {
-          return d!== 'period';
+      $scope.getSeriesNames = function () {
+        return d3.keys($scope.dataset[0])
+          .filter(function (d) {
+            return d !== 'period';
+          });
+      };
+
+      $scope.parseDatasetDates = function () {
+        $scope.dataset.forEach(function (d) {
+          d.period = parseDate(d.period);
         });
+      };
 
-      $scope.dataset.forEach(function (d) {
-        d.period = parseDate(d.period);
-      });
+      $scope.parseDatasetDates();
 
-      var sources = seriesNames.map(function (source) {
-        return {
-          name: source,
-          values: $scope.dataset.map(function (d) {
-            return {
-              period: d.period,
-              amount: d[source]
-            };
+      $scope.getSources = function () {
+        return seriesNames.map(function (source) {
+          return {
+            name: source,
+            values: $scope.dataset.map(function (d) {
+              return {
+                period: d.period,
+                amount: d[source]
+              };
+            })
+          };
+        });
+      };
+
+      var seriesNames = $scope.getSeriesNames();
+      var sources = $scope.getSources();
+      var source;
+
+      $scope.setDomains = function () {
+        x.domain(d3.extent($scope.dataset, function (d) {
+          return d.period;
+        })).nice();
+        y.domain([
+          d3.min(sources, function (s) {
+            return d3.min(s.values, function (v) {
+              return v.amount;
+            });
+          }),
+          d3.max(sources, function (s) {
+            return d3.max(s.values, function (v) {
+              return v.amount;
+            });
           })
-        };
-      });
+        ]).nice();
+      };
 
-      x.domain(d3.extent($scope.dataset, function (d) {
-        return d.period;
-      })).nice();
+      $scope.renderInitialGraph = function () {
+        $scope.setDomains();
 
-      y.domain([
-        d3.min(sources, function (s) {
-          return d3.min(s.values, function (v) {
-            return v.amount;
+        svg.append('g')
+          .attr({
+            'class': 'x axis',
+            transform: 'translate(0,' + height + ')'
+          })
+          .call(xAxis);
+
+        svg.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis)
+          .append('text')
+          .attr({
+            transform: 'rotate(-90)',
+            y: 6,
+            dy: '.71em'
+          })
+          .style('text-anchor', 'end')
+          .text($scope.selectedMetric);
+
+        source = svg.selectAll('.source')
+          .data(sources)
+          .enter().append('g')
+          .attr('class', function (d, i) {
+            return 'series ' + seriesNames[i];
           });
-        }),
-        d3.max(sources, function (s) {
-          return d3.max(s.values, function (v) {
-            return v.amount;
+
+        source.append('path')
+          .attr({
+            'class': 'line',
+            d: function (d) {
+              return line(d.values);
+            }
           });
-        })
-      ]).nice();
 
-      svg.append('g')
-        .attr({
-          'class': 'x axis',
-          transform: 'translate(0,' + height + ')'
-        })
-        .call(xAxis);
+        source.append('g').selectAll('circle')
+          .data(function (d) {
+            return d.values;
+          })
+          .enter()
+          .append('circle')
+          .attr({
+            'class': 'datapoint',
+            r: Layout.circleRadius,
+            cx: function (d) {
+              return x(d.period);
+            },
+            cy: function (d) {
+              return y(d.amount);
+            }
+          });
+      };
 
-      svg.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis)
-        .append('text')
-        .attr({
-          transform: 'rotate(-90)',
-          y: 6,
-          dy: '.71em'
-        })
-        .style('text-anchor', 'end')
-        .text($scope.selectedMetric);
+      $scope.renderInitialGraph();
 
-      var source = svg.selectAll('.source')
-        .data(sources)
-        .enter().append('g')
-        .attr('class', function (d, i) {
-          return 'series ' + seriesNames[i];
-        });
+      $scope.updateGraph = function () {
+        console.log('updating graph');
+        $scope.params.startDate = $scope.startDate;
+        // update data
+        $scope.dataset = Data.getPeriodData(result.data, $scope.startDate, $scope.endDate, ['facebook', 'twitter'], 'advocacy');
 
-      console.log(sources);
+        $scope.parseDatasetDates();
 
-      source.append('path')
-        .attr({
-          'class': 'line',
-          d: function (d) {
-            console.log(d);
+        sources = $scope.getSources();
+
+        // update domains
+        $scope.setDomains();
+
+        // update lines
+        var sel = svg.selectAll('.series')
+          .data(sources);
+
+        sel
+          .select('path')
+          .transition()
+          .ease(Layout.easeMethod)
+          .duration(Layout.dataUpdateDuration)
+          .attr('d', function (d) {
             return line(d.values);
-          }
-        });
+          });
+
+        sel
+          .selectAll('.datapoint')
+          .data(function (d) {
+            return d.values;
+          })
+          .transition()
+          .ease(Layout.easeMethod)
+          .duration(Layout.dataUpdateDuration)
+          .attr({
+            cx: function (d) {
+              return x(d.period);
+            },
+            cy: function (d) {
+              return y(d.amount);
+            }
+          });
+
+
+        console.log(Parameters);
+      };
+
+      $scope.$watch('Parameters.StartDate', function () {
+        console.log('updated parameters!');
+      });
 
     });
   });
