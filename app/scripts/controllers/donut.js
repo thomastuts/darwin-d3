1711bc131cfa39146608ad342469276bdba0759b
Thomas Tuts <thomas.tuts@gmail.com>
2013-10-21 14:36:54 +0200

    Scaffolding for update pattern

--- app/scripts/controllers/donut.js
+++ app/scripts/controllers/donut.js
@@ -2,8 +2,9 @@
 
 angular.module('darwinD3App')
   .controller('DonutCtrl', function ($scope, Data, Layout, Parameters) {
+    $scope.params = Parameters.params;
     Data.getData().then(function (result) {
-      $scope.dataset = Data.getDonutData(result.data, '2013-09-01', '2013-09-30', 'facebook', ['advocacy', 'appreciation', 'awareness']);
+      $scope.dataset = Data.getDonutData(result.data, $scope.params.startDate, $scope.params.endDate, 'facebook', ['advocacy', 'appreciation', 'awareness']);
 
       var width = 960,
         height = 500,
@@ -28,19 +29,36 @@ angular.module('darwinD3App')
         .enter().append("g")
         .attr("class", "arc");
 
-      g.append("path")
-        .attr({
-          d: arc,
-          'class': function (d) {
-            return d.data.metric;
-          }
-        });
+      $scope.renderInitialGraph = function () {
+        g.append("path")
+          .attr({
+            d: arc,
+            'class': function (d) {
+              return d.data.metric;
+            }
+          });
 
-      g.append("text")
-        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-        .attr("dy", ".35em")
-        .style("text-anchor", "middle")
-        .text(function(d) { return d.data.metric; });
+        g.append("text")
+          .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d.data.metric; });
+      };
+
+      $scope.renderInitialGraph();
+
+      $scope.updateGraph = function () {
+        $scope.dataset = Data.getDonutData(result.data, $scope.params.startDate, $scope.params.endDate, 'facebook', ['advocacy', 'appreciation', 'awareness']);
+
+        var sel = svg.selectAll(".arc")
+          .data(pie($scope.dataset));
+
+        var arcs = sel.selectAll('path');
+      };
+
+      $scope.$watch('params', function (newValue, oldValue) {
+        $scope.updateGraph();
+      }, true);
 
     });
   });
