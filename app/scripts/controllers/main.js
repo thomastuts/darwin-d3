bd72a5c03436d6bb37f0bdd85fffe042d4ce17c1
Thomas Tuts <thomas.tuts@gmail.com>
2013-10-22 10:59:56 +0200

    Remove Fixtures service from controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('darwinD3App')
-  .controller('MainCtrl', function ($scope, Fixtures, Layout, Data) {
+  .controller('MainCtrl', function ($scope, Layout, Data) {
   });
