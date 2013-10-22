b3d0250dc0047323ba6f32b44730f77aab7421d7
Thomas Tuts <thomas.tuts@gmail.com>
2013-10-22 13:37:01 +0200

    Re-arrange multibar & donut graph

--- app/scripts/controllers/multibar.js
+++ app/scripts/controllers/multibar.js
@@ -15,8 +15,8 @@ angular.module('darwinD3App')
       });
 
       var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 800 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        width = 400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
 
       var x = d3.time.scale()
         .range([0, width]);
